43d5bb882850c2fc20a3b5baf2bce3737c70b34b
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-07-16 17:51:09 +0200

    first-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/routes/auth.routes').then(r => r.authRoutes)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/routes/heroes.routes').then(r => r.heroesRoutes)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
---
+++ src/app/heroes/routes/heroes.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { LayoutPageComponent } from '../pages/layout-page/layout-page.component';
+import { NewPageComponent } from '../pages/new-page/new-page.component';
+import { ListPageComponent } from '../pages/list-page/list-page.component';
+import { SearchPageComponent } from '../pages/search-page/search-page.component';
+import { HeroPageComponent } from '../pages/hero-page/hero-page.component';
+
+
+export const heroesRoutes: Routes = [
+  {
+    path: '' ,
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
+]
+
